0c9e28474f158e77b1a3675fd588ae379dc7cf26
MdDubey <md@whatever.com>
2014-03-27 15:11:21 +0530

    validations done for proper input

--- ikebana.js
+++ ikebana.js
@@ -1,7 +1,12 @@
 var inputParser = require('./lib/user-input');
 var main = function (argv) {
-    var userInput = inputParser.parseInput(argv.slice(2));
-    inputParser.processInput(userInput);
+    try{
+        var userInput = inputParser.parseInput(argv.slice(2));
+        inputParser.processInput(userInput);
+    }
+    catch (err){
+        console.log(err);
+    }
 }
 
 main(process.argv);
