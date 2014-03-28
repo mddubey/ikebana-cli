895b6671f21e119619184d493c8ee1d27bb0838d
MdDubey <md@whatever.com>
2014-03-28 14:32:14 +0530

    Extra work removed, global intallation functionality removed

--- test/input-parser-test.js
+++ test/input-parser-test.js
@@ -5,7 +5,7 @@ describe('#InputParser', function () {
     describe('parseInput', function () {
         it('should parse input and identify task and package', function () {
             var inputs = ['install', 'errorButton'];
-            var expected = {task: 'install', packageInfo: 'errorButton', isGlobal: false};
+            var expected = {task: 'install', packageInfo: 'errorButton'};
             var actual = userInput.parseInput(inputs);
 
             assert.deepEqual(expected, actual);
@@ -14,21 +14,11 @@ describe('#InputParser', function () {
 
     describe('parseInput', function () {
         it('should identify that given task has to be performed globally', function () {
-            var inputs = ['uninstall', '-g', 'button'];
-            var expected = {task: 'uninstall', packageInfo: 'button', isGlobal: true};
+            var inputs = ['uninstall', 'button'];
+            var expected = {task: 'uninstall', packageInfo: 'button'};
             var actual = userInput.parseInput(inputs);
 
             assert.deepEqual(expected, actual);
         })
-    })
-
-    describe('parseInput', function () {
-        it('should identify package and options in any order', function () {
-            var inputs = ['install', 'button', '-g'];
-            var expected = {task: 'install', packageInfo: 'button', isGlobal: true};
-            var actual = userInput.parseInput(inputs);
-
-            assert.deepEqual(expected, actual);
-        })
-    })
-})
+    });
+});
